9c05e162cbf4c90dc6bedfe5580b18b36fb21ac8
Dima Melnychenko <aligator4511@gmail.com>
2026-01-05 20:44:05 +0200

    Зроблено 10 змін (Комітів)

--- GIT_HM/Program.cs
+++ GIT_HM/Program.cs
@@ -6,3 +6,6 @@ Console.WriteLine("Четвертий коміт (Розробник 1)");
 Console.WriteLine("Пятий коміт (Розробник 1)");
 Console.WriteLine("Шостий коміт (Розробник 1)");
 Console.WriteLine("Сьомий коміт (Розробник 1)");
+Console.WriteLine("Восьмий коміт (Розробник 1)");
+Console.WriteLine("Девятий коміт (Розробник 1)");
+Console.WriteLine("Десятий коміт (Розробник 1)");
